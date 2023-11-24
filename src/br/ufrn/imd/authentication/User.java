4e39e0f63cf22a397b35b6d48e2e144e3b14c2d8
daniel <danielnobre09@gmail.com>
2023-11-24 17:09:01 -0300

    user getters and setters

--- src/br/ufrn/imd/authentication/User.java
+++ src/br/ufrn/imd/authentication/User.java
@@ -1,5 +1,49 @@
 package br.ufrn.imd.authentication;
 
 public class User {
-
+	private String username;
+	private String email;
+	private String password;
+	private int id;
+	private boolean auth;
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword () {
+		return password;
+	}
+	
+	public void serId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setAuth(boolean auth) {
+		this.auth = auth;
+	}
+	
+	public boolean getAuth() {
+		return auth;
+	}
 }
