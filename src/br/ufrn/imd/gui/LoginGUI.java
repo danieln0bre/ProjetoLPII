0f83958f37fc624d01528edd8c4286e359d682b5
Gsdvl <gsdvl2004@gmail.com>
2023-11-27 15:54:56 -0300

    Atualização LoguinGUI

--- src/br/ufrn/imd/gui/LoginGUI.java
+++ src/br/ufrn/imd/gui/LoginGUI.java
@@ -1,5 +1,31 @@
 package br.ufrn.imd.gui;
+import javax.swing.*;
+public class LoginGUI extends JFrame {
+    public LoginGUI(){
+        setTitle("MediaPlayerApp");
+        setVisible(true);
+        setSize(800,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
 
-public class LoginGUI {
+        JButton loginButton = new JButton("login");
+        JButton registerButton = new JButton("register");
+        JTextField loginField = new JTextField(20);
+        JPasswordField passwordField = new JPasswordField(20);
 
+        //loginButtonConfig
+        loginButton.setBounds(300,300,100,20);
+
+        //registerButtonConfig
+        registerButton.setBounds(400,300,100,20);
+
+        //loginFieldConfig
+        loginField.setBounds(300,350,400,300);
+        //passwordFieldConfig
+
+        add(loginButton);
+        add(registerButton);
+        add(loginField);
+    }
 }
--- src/br/ufrn/imd/main/MediaPlayerApp.java
+++ src/br/ufrn/imd/main/MediaPlayerApp.java
@@ -1,17 +1,19 @@
 package br.ufrn.imd.main;
-import br.ufrn.imd.authentication.User;
-import br.ufrn.imd.authentication.UserManager;
-import br.ufrn.imd.authentication.CommonUser;
+//import br.ufrn.imd.authentication.User;
+//import br.ufrn.imd.authentication.UserManager;
+//import br.ufrn.imd.authentication.CommonUser;
+import br.ufrn.imd.gui.LoginGUI;
 
 public class MediaPlayerApp {
 	public static void main (String[] args) {
-		
-		UserManager manager = new UserManager();
+
+		new LoginGUI();
+		/*UserManager manager = new UserManager();
 		manager.registerUser("common", "daniel", "daniel@teste.com", "teste");
 		manager.loadUsers();
 		System.out.println(manager.loadUsers());
-		// CommonUser user = new CommonUser();		
+		CommonUser user = new CommonUser();
 		
-		// user.addDirectory();
+		user.addDirectory();*/
 	}
 }
