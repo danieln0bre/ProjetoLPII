34ea91d4984193e0812690aa79a08ba2ed92b040
daniel <danielnobre09@gmail.com>
2023-11-27 19:56:44 -0300

    correção

--- src/br/ufrn/imd/main/MediaPlayerApp.java
+++ src/br/ufrn/imd/main/MediaPlayerApp.java
@@ -1,24 +1,16 @@
 package br.ufrn.imd.main;
-<<<<<<< HEAD
 import br.ufrn.imd.authentication.UserManager;
-=======
-//import br.ufrn.imd.authentication.User;
-//import br.ufrn.imd.authentication.UserManager;
-//import br.ufrn.imd.authentication.CommonUser;
 import br.ufrn.imd.gui.LoginGUI;
->>>>>>> 0f83958f37fc624d01528edd8c4286e359d682b5
 
 public class MediaPlayerApp {
 	public static void main (String[] args) {
 
 		new LoginGUI();
-		/*UserManager manager = new UserManager();
+		UserManager manager = new UserManager();
 		manager.registerUser("common", "daniel", "daniel@teste.com", "teste");
 		manager.registerUser("vip", "gabriel", "gabriel@teste.com", "teste");
 		manager.loadUsers();
 		System.out.println(manager.loadUsers());
-		CommonUser user = new CommonUser();
-		
-		user.addDirectory();*/
+	
 	}
 }
